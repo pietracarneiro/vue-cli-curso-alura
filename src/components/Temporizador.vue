9e38d0986758289a2243f801a7add336ade0ee95
Pietra Carneiro <pietrasc98@gmail.com>
2022-12-22 10:12:24 -0300

    Adicionando interfaces e dist

--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -1,51 +1,58 @@
 <template>
-    <div class="is-flex is-align-items-center is-justify-content-space-between">
-        <Cronometro :tempoEmSegundos="tempoEmSegundos" />
-        <button class="button" @click="iniciar" :disabled="cronometroRodando">
-            <span class="icon">
-                <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-        </button>
-        <button class="button" @click="finalizar" :disabled="!cronometroRodando">
-            <span class="icon">
-                <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-        </button>
-    </div>
+  <section
+    class="is-flex is-align-items-center is-justify-content-space-between"
+  >
+    <Cronometro :tempoEmSegundos="tempoEmSegundos" />
+    <Botao
+      @clicado="iniciar"
+      icone="fas fa-play"
+      texto="play"
+      :desabilitado="cronometroRodando"
+    />
+    <Botao
+      @clicado="finalizar"
+      icone="fas fa-stop"
+      texto="stop"
+      :desabilitado="!cronometroRodando"
+    />
+  </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import Cronometro from "./Cronometro.vue";
+import Botao from "./Botao.vue";
 
 export default defineComponent({
-    name: "Temporizador",
-    components: {
-        Cronometro
+  name: "Temporizador",
+  emits: ["aoTemporizadorFinalizado"],
+  components: {
+    Cronometro,
+    Botao
+  },
+  data() {
+    return {
+      tempoEmSegundos: 0,
+      cronometro: 0,
+      cronometroRodando: false,
+    };
+  },
+  methods: {
+    iniciar(): void {
+      // começar a contagem
+      // 1s = 1000ms
+      this.cronometroRodando = true;
+      this.cronometro = setInterval(() => {
+        this.tempoEmSegundos += 1;
+      }, 1000);
+      console.log("iniciando");
     },
-    data () {
-        return {
-            tempoEmSegundos: 0,
-            cronometro: 0,
-            cronometroRodando: false
-        };
+    finalizar(): void {
+      this.cronometroRodando = false;
+      clearInterval(this.cronometro);
+      this.$emit("aoTemporizadorFinalizado", this.tempoEmSegundos);
+      this.tempoEmSegundos = 0;
     },
-    methods: {
-        iniciar(): void {
-            // começar a contagem
-            // 1s = 1000ms
-            this.cronometroRodando = true
-            this.cronometro = setInterval(() => {
-                this.tempoEmSegundos += 1;
-            }, 1000);
-            console.log("iniciando");
-        },
-        finalizar(): void {
-            this.cronometroRodando = false
-            clearInterval(this.cronometro);
-        },
-    },
+  },
 });
 </script>
