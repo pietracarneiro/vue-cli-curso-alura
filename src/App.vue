533eb28593f3a95afbfcff4d367ed58cdc32e206
Pietra Carneiro <pietrasc98@gmail.com>
2023-06-28 17:59:52 -0300

    WIP: criando componente de notificações

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
     </div>
     <div class="column is-three-quarter conteudo">
       <!-- AQUI VAI A VIEW CORRESPONDENTE -->
+      <Notificacoes />
       <router-view></router-view>
     </div>
   </main>
@@ -13,11 +14,13 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import BarraLateral from "./components/BarraLateral.vue";
+import Notificacoes from "./components/Notificacoes.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     BarraLateral,
+    Notificacoes
   },
   data() {
     return {
---
+++ src/components/Notificacoes.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="notificacoes">
+        <article class="message is-success">
+            <div class="message-header">Atenção!</div>
+            <div class="message-body">
+                Aqui vai um texto bacana!
+            </div>
+        </article>
+        <article class="message is-warning">
+            <div class="message-header">Atenção!</div>
+            <div class="message-body">
+                Aqui vai um texto bacana!
+            </div>
+        </article>
+        <article class="message is-danger">
+            <div class="message-header">Atenção!</div>
+            <div class="message-body">
+                Aqui vai um texto bacana!
+            </div>
+        </article>
+    </div>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from "vue"
+
+export default defineComponent({
+    name: "Notificações"
+})
+
+</script>
+
+<style scoped>
+.notificacoes {
+    position: absolute;
+    right: 0;
+    width: 300px;
+    padding: 8px;
+}
+</style>
